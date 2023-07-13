fe0ee0e1a95761525071feabc12680b5e61d2ed4
Cuckss <122050270+Cuckss@users.noreply.github.com>
2023-07-13 18:05:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function threeSum(arr, target) {
-// write your code here
+arr.sort((a,b)=>a-b);
+	let closestSum=infinity;
+	for(let i=0;i<arr.length-2;i++){
+		let low=i+1;
+		let high=arr.length-1;
+		while(low<high){
+			let currSum=arr[i]+arr[low]+arr[high];
+			if(Math.abs(target-currSum)<Math.abs(target-closestSum)){
+				closestSum=currSum;
+			}
+			if(currSum<target){
+				low++;
+			}
+		  else if(currSum>target){
+			  high--;
+		  }
+			else{
+				return closestSum
+			}
+		}
+		
+	}
+	return closestSum;
   
 }
 
